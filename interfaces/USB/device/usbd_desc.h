b0ae02b39da69ffbc2f1469e569c42feacdd24ec
dangfan <dangf09@gmail.com>
2021-02-09 12:55:33 +0800

    use temp usb id for mac users

--- interfaces/USB/device/usbd_desc.h
+++ interfaces/USB/device/usbd_desc.h
@@ -4,8 +4,8 @@
 
 #include <usbd_def.h>
 
-#define USBD_VID 0x20A0
-#define USBD_PID 0x42D4
+#define USBD_VID 0x1677
+#define USBD_PID 0x0025
 
 /** Descriptor for the Usb device. */
 extern const USBD_DescriptorsTypeDef usbdDescriptors;
